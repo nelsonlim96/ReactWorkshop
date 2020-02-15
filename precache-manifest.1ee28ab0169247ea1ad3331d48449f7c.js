31505eaff289d69ec89568e57a5c7164658decbb
Nelson Lim <30296067+nelsonlim96@users.noreply.github.com>
2020-02-15 18:21:47 +0800

    Updates

---
+++ precache-manifest.1ee28ab0169247ea1ad3331d48449f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7306998437e9ede68319c09cadf38310",
+    "url": "/React-P1/index.html"
+  },
+  {
+    "revision": "05f88e3db915b87dfea3",
+    "url": "/React-P1/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2c912dd6350a51098421",
+    "url": "/React-P1/static/css/main.903ac66b.chunk.css"
+  },
+  {
+    "revision": "05f88e3db915b87dfea3",
+    "url": "/React-P1/static/js/2.0cc5b7fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-P1/static/js/2.0cc5b7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c912dd6350a51098421",
+    "url": "/React-P1/static/js/main.49d3d652.chunk.js"
+  },
+  {
+    "revision": "a246db2f8028eaa3df71",
+    "url": "/React-P1/static/js/runtime-main.ee9dbc46.js"
+  }
+]);
